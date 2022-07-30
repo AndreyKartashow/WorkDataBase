bbc98807dc6fb795789e11f71fc91f45b38da585
Andrey <patron6239@gmail.com>
2022-07-30 22:46:26 +0300

    Adding folder HomeWork_1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько людей вы хотите сгененировать?");
+            int countPerson = InputCountPerson();
+            Console.WriteLine("Сколько машин вы хотите сгененировать?");
+            int countTransport = InputCountPerson();
+
+            Person person = new Person();
+            Transport transport = new Transport();
+
+            person.ScriptPerson(countPerson);
+            transport.ScriptTransport(countTransport);
+        }
+
+        private static int InputCountPerson()
+        {
+            try
+            {
+                int countPerson = int.Parse(Console.ReadLine());
+                return countPerson;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Вы ввели неверные данные, попробуйте еще раз");
+                return InputCountPerson();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели неверные данные, попробуйте еще раз");
+                return InputCountPerson();
+            }
+        }
+    }
+}
