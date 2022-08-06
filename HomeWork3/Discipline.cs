f85c96c955ed811ec1aaa9bc4849a3f4ed35e165
Andrey <patron6239@gmail.com>
2022-08-06 14:25:55 +0300

    Change file discipline

--- HomeWork3/Discipline.cs
+++ HomeWork3/Discipline.cs
@@ -11,15 +11,16 @@ namespace DataBase
             Random random = new Random();
             for (int i = 0; i < count; i++)
             {
+                string nameDiscipline = name[random.Next(name.Length)];
                 int idDiscipline = random.Next(10000, 20000);
                 int plan = random.Next(48, 78);
                 int idOccupationDiscipline = random.Next(70000, 80000);
                 int idOccupation = random.Next(0, 10000);
                 
-                text += $"{idDiscipline};{name};{plan};{idOccupationDiscipline};{idOccupation}\n";
+                text += $"{idDiscipline};{nameDiscipline};{plan};{idOccupationDiscipline};{idOccupation}\n";
             }
 
-            File.WriteAllText("ScriptDiscipline.csv", text);
+            File.WriteAllText("Discipline.csv", text);
         }
     }
 }
